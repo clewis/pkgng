f1c81692a6aa271a12744f1e40604297346073aa
Craig Lewis <clewis@centraldesktop.com>
2015-02-18 13:09:49 -0800

    Upgrade line to 0.6.0; version bump

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email 'douglas@douglasthrift.net'
 license          'Apache 2.0'
 description      'Installs/Configures pkgng'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.1.0'
+version          '0.1.1'
 
 supports 'freebsd', '>= 8.4'
 
-depends 'line', '~> 0.5.2'
+depends 'line', '= 0.6.0'
